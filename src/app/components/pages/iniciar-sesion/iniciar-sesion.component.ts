a76e90ffdb240870da50bda0d259445e19a664c3
RodrigoSY <sihues3@gmail.com>
2025-10-17 00:23:54 -0500

    correcion de advertencias

--- src/app/components/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/components/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
